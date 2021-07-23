9e8386b0075e7e34ae9415263bf50207c82d53e2
JordanSussman <jordan_sussman@cargill.com>
2021-07-22 19:19:53 -0500

    fix comments

--- template/template.go
+++ template/template.go
@@ -9,10 +9,10 @@ import "github.com/go-vela/types/yaml"
 // Engine represents the interface for Vela integrating
 // with the different supported template engines.
 type Engine interface {
-	// Render defines a function that combines
-	// the template with the step.
+	// RenderBuild defines a function that combines
+	// the template with the build.
 	RenderBuild(template string, step *yaml.Step) (yaml.StepSlice, error)
-	// Render defines a function that combines
+	// RenderStep defines a function that combines
 	// the template with the step.
 	RenderStep(template string, step *yaml.Step) (yaml.StepSlice, error)
 }
