e42fb0f175a90a984b056ce875c8ff3c94e34865
Kelly Merrick <kelly.merrick@target.com>
2021-08-05 08:29:26 -0500

    ignore line length for linter

--- registry/github/parse.go
+++ registry/github/parse.go
@@ -39,8 +39,8 @@ func (c *client) Parse(path string) (*registry.Source, error) {
 
 	// ensure org, repo and filename parts exist
 	// nolint: gomnd // ignore magic number
-	// nolint: lll // ignore long line length due to error message
 	if len(parts) < 3 {
+		// nolint: lll // ignore long line length due to error message
 		return &registry.Source{}, fmt.Errorf("invalid template source %s, must contain org/repo/path_to_template", path)
 	}
 
